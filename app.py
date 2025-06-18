94bfc63518d8c1afbccee355f07764831b6a2602
Student <heath@heathconklin.com>
2025-06-18 09:52:02 -0400

    added env to git and modified book looping

--- app.py
+++ app.py
@@ -6,17 +6,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    print("libraryObj")
     return render_template("index.html")
 
 @app.route("/Library")
 def library():
     allLibraries = File.read_file('library.txt')
     libraryObj = json.loads(allLibraries)
+    print(libraryObj)
     return render_template("library.html", libraries=libraryObj)
 
 @app.route("/Books")
 def book():
-    return render_template("book.html")
+    allLibraries = File.read_file('library.txt')
+    libraryObj = json.loads(allLibraries)
+    print(libraryObj)
+    return render_template("library.html", libraries=libraryObj)
 
 @app.route("/Photo")
 def photo():
